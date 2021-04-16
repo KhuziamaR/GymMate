f7592ec98804179ec6fc9bff5edf52d5fa4520f8
KhuziamaR <krehman000@citymail.cuny.edu>
2021-04-15 20:36:33 -0400

    Create Xcode Project

---
+++ GymMate/GymMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymMate
+//
+//  Created by Kinza Rehman on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
