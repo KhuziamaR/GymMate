ec8f6ab14cb245ab1f872829f969f5599fe9f20a
KhuziamaR <krehman000@citymail.cuny.edu>
2021-04-15 22:40:38 -0400

    Designed the sign in / sign up page

--- GymMate/GymMate/ViewController.swift
+++ GymMate/GymMate/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orLabel: UILabel!
+    @IBOutlet weak var termsOfServiceLabel: UILabel!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var signInGoogleButton: UIButton!
+    @IBOutlet weak var signInFacebookButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       setupUI()
     }
-
+    
+    func setupUI(){
+        
+        let title = "Create a new account"
+        let subTitle = "\n\n\n\n\nLorem ipsum fdf fdsf dfsdf fdsf."
+        
+        let attributedText = NSMutableAttributedString(string: title,attributes: [NSAttributedString.Key.font : UIFont.init(name: "Didot", size: 28)!, NSAttributedString.Key.foregroundColor : UIColor.black
+        ])
+        let attributedSubText = NSMutableAttributedString(string: subTitle,attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.45)
+        ])
+        attributedText.append(attributedSubText)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.length))
+        
+        titleLabel.numberOfLines = 0
+        titleLabel.attributedText = attributedText
+    }
 
 }
 
