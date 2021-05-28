efda9e012113bc15e8172bd92d77dce818c180f9
Khuziama Rehman <rehmankhuziama@gmail.com>
2021-05-27 23:29:14 -0400

    User can post gym mate request, which gets stored onto database

--- GymMate/GymMate/CameraViewController.swift
+++ GymMate/GymMate/CameraViewController.swift
@@ -6,50 +6,140 @@
 //
 
 import UIKit
+import ProgressHUD
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseDatabase
+
+
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    var ref: DatabaseReference!
 
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var caption: UITextField!
-    @IBAction func PostSubmit(_ sender: Any) {
-        
-    }
+    @IBOutlet weak var photo: UIImageView!
+
+    @IBOutlet weak var captionTextView: UITextField!
     
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var zipcode: UITextField!
+    var selectedImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        photo.isUserInteractionEnabled = true
+        self.ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func onCameraButton(_ sender: Any) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            picker.sourceType = .camera
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handlePost()
+    }
+    
+    func handlePost() {
+        if selectedImage != nil {
+           self.shareButton.isEnabled = true
+            self.shareButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         } else {
-            picker.sourceType = .photoLibrary
+           self.shareButton.isEnabled = true
+            self.shareButton.backgroundColor = .lightGray
+
         }
-        present(picker, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @objc func handleSelectPhoto() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        view.endEditing(true)
+        ProgressHUD.show("Waiting...", interaction: false)
+        guard (Auth.auth().currentUser?.uid) != nil else { return }
+        if let profileImg = self.selectedImage, let imageData = profileImg.jpegData(compressionQuality: 0.1) {
+            let metaData = StorageMetadata()
+            metaData.contentType = "image/jpg"
+            let photoIdString = NSUUID().uuidString
+            print(photoIdString)
+            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOF_REF).child("posts").child(photoIdString)
+            storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    ProgressHUD.showError(error!.localizedDescription)
+                    return
+                }
+                storageRef.downloadURL { url, error in
+//                    print(url!.absoluteString)
+                    self.sendDataToDatabase(photoUrl: url!.absoluteString)
+                }
+            })
+        } else {
+            ProgressHUD.showError("Profile Image can't be empty")
+        }
+    }
+    @IBAction func onCameraButton(_ sender: Any) {
+          let picker = UIImagePickerController()
+          picker.delegate = self
+          picker.allowsEditing = true
+          
+          if UIImagePickerController.isSourceTypeAvailable(.camera){
+              picker.sourceType = .camera
+          } else {
+              picker.sourceType = .photoLibrary
+          }
+          present(picker, animated: true, completion: nil)
+      }
+    
+    @IBAction func shareButton_TouchUpInside(_ sender: Any) {
+        print("share button pressed")
+    }
+    @IBAction func remove_TouchUpInside(_ sender: Any) {
+        clean()
+        handlePost()
+    }
+    
+    func sendDataToDatabase(photoUrl: String) {
+        let postsReference = self.ref.child("posts")
+        let newPostId = postsReference.childByAutoId().key!
+        let newPostReference = postsReference.child(newPostId)
+        newPostReference.setValue(["photoUrl": photoUrl, "caption": captionTextView.text!,"zipcode": zipcode.text!], withCompletionBlock: {
+            (error, ref) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            ProgressHUD.showSuccess("Success")
+           self.clean()
+            self.tabBarController?.selectedIndex = 0
+        })
+    }
+    
+    func clean() {
+        self.captionTextView.text = ""
+//        self.photo.image = UIImage(named: "placeholder-photo")
+        self.selectedImage = nil
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.editedImage] as! UIImage
-        
-        imageView.image = image
-        
-        dismiss(animated: true, completion: nil)
-    }
+           let image = info[.editedImage] as! UIImage
+           selectedImage = image
+           photo.image = image
+           
+           dismiss(animated: true, completion: nil)
+       }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        print("did Finish Picking Media")
+//        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+//            selectedImage = image
+//            photo.image = image
+//        }
+//        dismiss(animated: true, completion: nil)
+//    }
+    
 }
+
---
+++ GymMate/GymMate/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  GymMate
+//
+//  Created by Khuziama Rehman on 5/27/21.
+//
+
+
+import Foundation
+struct Config {
+    static var STORAGE_ROOF_REF = "gs://gymmate-e5498.appspot.com"
+}
